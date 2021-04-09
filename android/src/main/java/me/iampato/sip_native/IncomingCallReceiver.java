18d1742f1ccbc1a1c39f3142ad25639a58a478e2
Iampato <858wpwaweru@gmail.com>
2021-04-10 00:02:03 +0300

    added an incomming call receiver

---
+++ android/src/main/java/me/iampato/sip_native/IncomingCallReceiver.java
@@ -0,0 +1,14 @@
+package me.iampato.sip_native;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.sip.SipAudioCall;
+import android.net.sip.SipProfile;
+
+public class IncomingCallReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
