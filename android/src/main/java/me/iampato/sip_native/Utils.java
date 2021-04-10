d09e884608ee9a1b5cc8769e218f88b99d5c69fc
Iampato <858wpwaweru@gmail.com>
2021-04-10 15:19:24 +0300

    added utils

--- android/src/main/java/me/iampato/sip_native/Utils.java
+++ android/src/main/java/me/iampato/sip_native/Utils.java
@@ -1,2 +1,29 @@
-package me.iampato.sip_native;public class Utils {
+package me.iampato.sip_native;
+
+import android.content.pm.PackageManager;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
+
+public class Utils {
+    public boolean checkPermissions(String[] permissions, ActivityPluginBinding activityPluginBinding) {
+        int result;
+        List<String> listPermissionsNeeded = new ArrayList<>();
+        for (String p : permissions) {
+            result = ContextCompat.checkSelfPermission(activityPluginBinding.getActivity().getApplicationContext(), p);
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                listPermissionsNeeded.add(p);
+            }
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(activityPluginBinding.getActivity(), listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]), 1111);
+            return false;
+        }
+        return true;
+    }
 }
