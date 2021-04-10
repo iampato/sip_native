47b31bf0eb02df5ae09656756762576bf16d4947
Iampato <858wpwaweru@gmail.com>
2021-04-10 15:17:05 +0300

    added enums

--- android/src/main/java/me/iampato/sip_native/SipCallState.java
+++ android/src/main/java/me/iampato/sip_native/SipCallState.java
@@ -1,2 +1,13 @@
-package me.iampato.sip_native;public enum SipCallState {
+package me.iampato.sip_native;
+
+public enum SipCallState {
+    UNKNOWN,
+    ONCALLING,
+    ONRINGING,
+    ONRINGINGBACK,
+    ONREADYTOCALL,
+    ONCALLESTABLISHED,
+    ONCALLENDED,
+    ONCALLBUSY,
+    ONERROR,
 }
